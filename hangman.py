9d29b96f715d2614a3382aa3fbfba863551930ba
Arthur Gabriel <123274385+arthurgmv@users.noreply.github.com>
2024-03-07 22:25:32 -0300

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,104 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+lives = 6
+
+print('''
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+
+''')
+
+word_list = ["apple", "tiger", "lion", "pineapple", "mouse", "chocolate","acid", "thunder", "frog", "grow", "scream", "punk"]
+chosen_word = random.choice(word_list)
+
+display = []
+for letter in chosen_word:
+    display += "_"
+print(display)
+
+end_of_game = False
+
+while end_of_game == False:
+    guess = input("Tell me a letter, please ").lower()
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+
+    print(stages[lives])
+
+
